43c10cb230eedfe7630ab2d5c173f47ccd95d8be
Kevin Tan <kevin.tan@yale.edu>
2015-03-31 20:20:32 -0400

    donald added

--- main.py
+++ main.py
@@ -8,14 +8,25 @@ app.debug = True
 @app.route('/')
 def hello_world():
 
+
+	#Donald
+	req = urllib2.Request('http://donaldwoodson.tumblr.com', headers={'User-Agent' : "Magic Browser"}) 
+	con = urllib2.urlopen( req )
+	soup=BeautifulSoup(con.read())
+	d_title = soup.findAll('h2')[0].a.string
+	d_title = d_title[2:]
+	d_date = soup.findAll("div", { "class" : "column left" })[1].a.string
+	# print d_title
+	print d_date
+
 	# steven
 	req = urllib2.Request('https://kangexpress.wordpress.com', headers={'User-Agent' : "Magic Browser"}) 
 	con = urllib2.urlopen( req )
 	soup=BeautifulSoup(con.read())
 	s_title = soup.findAll('h1')[1].a.string
 	s_date = soup.time.string
-	print s_title
-	print s_date
+	# print s_title
+	# print s_date
 
 	# frank
 	req = urllib2.Request('http://www.frankjwu.com', headers={'User-Agent' : "Magic Browser"}) 
@@ -48,7 +59,7 @@ def hello_world():
 	# print kt_date
 
 
-	return render_template('consortium.html', f_title = f_title, f_date = f_date, j_title = j_title, j_date =j_date, k_title =k_title, k_date = k_date, s_title=s_title, s_date=s_date)
+	return render_template('consortium.html', f_title = f_title, f_date = f_date, j_title = j_title, j_date =j_date, k_title =k_title, k_date = k_date, s_title=s_title, s_date=s_date, d_title = d_title, d_date = d_date)
 
 if __name__ == '__main__':
     app.run()
